644a1013c3d548d5a742aa6800000ceb45074494
harsshhfs <harssh@headfitted.com>
2013-11-11 16:59:09 +0530

    added spree auth plugin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeDemoApp::Application.config.secret_token = 'e758c099aa38ca3d2bd7f5f4bf630e57be3a41f8392c818526f386e4ea5337495758642fdba33639a86b1705acc082abb20da796cd1eec0b03ddd7e50acd37be'
